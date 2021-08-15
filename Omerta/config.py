3d3a1a7be9763b1d2bde37cfd0fe38613316b150
iLLMannerSevs <54834910+iLLMannerSevs@users.noreply.github.com>
2021-08-15 11:37:58 -0700

    Create config.py
    template w/ example 'server: channel'

---
+++ Omerta/config.py
@@ -0,0 +1,14 @@
+class Config:
+    # Find your token here: https://server.nitrado.net/usa/developer/tokens
+    NITRADO_TOKEN = ""
+    # Discord bot token: https://discord.com/developers/applications
+    DISCORD_TOKEN = ""
+    # Bot prefix (not used at the current version)
+    BOT_PREFIX = ""
+    # PVP Kill Embed Image supports .png .jpg .gif
+    EMBED_IMAGE = "https://i.postimg.cc/KvZR79Tt/candle.gif"
+
+    # Recommended to have less than 5 (Enable "Developer Mode" to get your channel's ID)
+    SERVERS = {
+        1234567: 817922233640288297, # (Gameserver) Service ID: Discord Channel ID
+    }
